0b0c9069585526d7b77c887d9092c91e3bc42876
saasbook <saasbook@saasbook.(none)>
2012-07-13 20:06:20 +0200

    Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SCVirtualOperator::Application.config.secret_token = '5618825435c0270661101775c7f901e0859fa4e6e069a67639c27f57a06008de63976ba5eeaecc90304198858c4369278d25f4ca7482cdbe33e2554203baead4'
